75cfc4981aecdabd556ef31af274c75ebff63a3f
PENCHAL G <37048949+pennet@users.noreply.github.com>
2018-05-06 14:07:58 +0530

    Add files via upload

---
+++ Docs/NatarazNotes/OldJavaWorkSpace/Image-Upload/build/generated/src/org/apache/jsp/Upload_jsp.java
@@ -0,0 +1,91 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class Upload_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html; charset=ISO-8859-1");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \n");
+      out.write("\t\"http://www.w3.org/TR/html4/loose.dtd\">\n");
+      out.write("<html>\n");
+      out.write("<head>\n");
+      out.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">\n");
+      out.write("<title>File Upload to Database Demo</title>\n");
+      out.write("</head>\n");
+      out.write("<body>\n");
+      out.write("\t<center>\n");
+      out.write("\t\t<h1>File Upload to Database Demo</h1>\n");
+      out.write("\t\t<form method=\"post\" action=\"uploadServlet\" enctype=\"multipart/form-data\">\n");
+      out.write("\t\t\t<table border=\"0\">\n");
+      out.write("\t\t\t\t<tr>\n");
+      out.write("\t\t\t\t\t<td>First Name: </td>\n");
+      out.write("\t\t\t\t\t<td><input type=\"text\" name=\"firstName\" size=\"50\"/></td>\n");
+      out.write("\t\t\t\t</tr>\n");
+      out.write("\t\t\t\t<tr>\n");
+      out.write("\t\t\t\t\t<td>Last Name: </td>\n");
+      out.write("\t\t\t\t\t<td><input type=\"text\" name=\"lastName\" size=\"50\"/></td>\n");
+      out.write("\t\t\t\t</tr>\n");
+      out.write("\t\t\t\t<tr>\n");
+      out.write("\t\t\t\t\t<td>Portrait Photo: </td>\n");
+      out.write("\t\t\t\t\t<td><input type=\"file\" name=\"photo\" size=\"50\"/></td>\n");
+      out.write("\t\t\t\t</tr>\n");
+      out.write("\t\t\t\t<tr>\n");
+      out.write("\t\t\t\t\t<td colspan=\"2\">\n");
+      out.write("\t\t\t\t\t\t<input type=\"submit\" value=\"Save\">\n");
+      out.write("\t\t\t\t\t</td>\n");
+      out.write("\t\t\t\t</tr>\n");
+      out.write("\t\t\t</table>\n");
+      out.write("\t\t</form>\n");
+      out.write("\t</center>\n");
+      out.write("</body>\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
